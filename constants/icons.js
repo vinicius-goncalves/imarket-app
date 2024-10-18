0bd5145d21c471139dc031c188a5a99c91035818
Vinícius Gonçalves <oldz.registros@gmail.com>
2024-10-17 23:45:15 -0300

    added profile.png icon

--- constants/icons.js
+++ constants/icons.js
@@ -1,4 +1,5 @@
 import home from '../assets/icons/home.png';
+import profile from '../assets/icons/profile.png';
 import search from '../assets/icons/search.png';
 
-export default { home, search };
+export default { home, search, profile };
