7d426ef92f7b3e1dd2c4d3681539b73d4ad3bc9b
Vinícius Gonçalves <oldz.registros@gmail.com>
2024-10-17 23:48:15 -0300

    feat: created app home page

--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,38 @@
-import { Link } from 'expo-router';
+import { router } from 'expo-router';
 import React from 'react';
-import { Text, View } from 'react-native';
+import { SafeAreaView, ScrollView, Text, View } from 'react-native';
+import CustomButton from '../components/CustomButton';
 
-export default function Root() {
+export default function Home() {
     return (
-        <View className="flex-1 items-center justify-center">
-            <Text className="text-4xl">iMarket</Text>
-            <Text className="text-sm text-gray-700">Welcome to the iMarket!</Text>
-            <View className="my-2">
-                <Link href="/home" className="border border-red-500 p-2">
-                    Access Home
-                </Link>
-            </View>
-        </View>
+        <SafeAreaView className="h-full bg-red-600">
+            <ScrollView contentContainerStyle={{ height: '100%' }}>
+                <View className="flex justify-center items-center min-h-[85vh]">
+                    <Text className="text-white font-bold text-6xl">
+                        imarket
+                    </Text>
+                    <Text className="text-white -my-2.5">
+                        the best products are here
+                    </Text>
+                </View>
+                <View className="absolute w-screen bg-white bottom-0 rounded-t-xl p-6">
+                    <View className="flex flex-col">
+                        <CustomButton
+                            title="i already have an account"
+                            onPress={() => {
+                                router.push('/login');
+                            }}
+                        />
+                        <CustomButton
+                            title="create an account"
+                            outlined={true}
+                            onPress={() => {
+                                router.push('/signup');
+                            }}
+                        />
+                    </View>
+                </View>
+            </ScrollView>
+        </SafeAreaView>
     );
 }
